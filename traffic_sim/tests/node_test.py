f59f22b308e2502c0a70e1a93e8e59528512be97
Harvey Arbas <harveyarbas@Harvstalavista.local>
2018-09-07 19:34:25 +0800

    create unit test for nodes

---
+++ traffic_sim/tests/node_test.py
@@ -0,0 +1,12 @@
+import unittest
+import weakref
+from app.model.infra.node import Node
+
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
