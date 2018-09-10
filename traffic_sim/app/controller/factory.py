82a259fba52694eb413d89bd91140bf88217e653
Harvey Arbas <harveyarbas@Harvstalavista.local>
2018-09-10 12:12:22 +0800

    implement factory pattern for road models

---
+++ traffic_sim/app/controller/factory.py
@@ -0,0 +1,17 @@
+from app.model.infra.node import Node
+from app.model.infra.road import Link, Connector
+
+
+def create_node(x, y, dir_):
+    return Node(x, y, dir_)
+
+
+def create_link(src_x, src_y, dst_x, dst_y, lanes):
+    src = create_node(src_x, src_y, 'src')
+    dst = create_node(dst_x, dst_y, 'dst')
+
+    return Link(src, dst, lanes)
+
+
+def create_connector(src_road, dst_road):
+    return Connector(src_road, dst_road)
