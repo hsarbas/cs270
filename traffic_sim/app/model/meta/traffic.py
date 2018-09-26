ca211ab6868019db301ddf9269efa1a5f4c9bdf4
Harvey Arbas <harveyarbas@Harvstalavista.local>
2018-09-26 11:44:39 +0800

    add dispatcher class

--- traffic_sim/app/model/meta/traffic.py
+++ traffic_sim/app/model/meta/traffic.py
@@ -9,7 +9,17 @@ class Dispatcher(QObject):
     def __init__(self, road):
         super(Dispatcher, self).__init__(parent=None)
         self._road = weakref.ref(road)
+        self._agent_manager = None
 
     @property
     def road(self):
         return self._road()
+
+    def run(self, agent_manager):
+        if agent_manager:
+            self._agent_manager = weakref.ref(agent_manager)
+        else:
+            self._agent_manager = None
+
+    def _signal_callback(self):
+        pass
